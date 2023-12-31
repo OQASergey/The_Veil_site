4773aa00c6b0cb23369c606d7082ea646baf8180
OQASergey <sergey.qa.63@protonmail.com>
2023-12-31 17:59:36 +0400

    Добавлены файлы сайта

---
+++ index.php
@@ -0,0 +1,46 @@
+﻿<head lang="ru">
+
+    <link type="image/x-icon" href="assets/img/favicon.png" rel="shortcut icon">
+    <link type="Image/x-icon" href="assets/img/favicon.png" rel="icon">
+
+	<title>НРИ - the Veil</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="assets/css/vendor.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+
+
+
+</head>
+<body>
+
+
+<div class="container">
+	<div class="row title">
+	<div class="col-12">
+			<br><br>
+<p style="color: #fff; font-size: 35px;">ПРЕДУПРЕЖДЕНИЕ</p>
+				<div class="line"></div>
+										<br>
+<p style="color: #fff; font-size: 20px;">Данный сайт является небольшим справочным ресурсом для личного использования по настольной ролевой игре the Veil (авторы: Фрейзер и Кайл Симмонс). Сайт ничего не пропогандирует и не распространяет. Публиковать ссылку на текущий домен, поддомены и страницы на домене или поддомене в любом другом ресурсе строго воспрещается.</p>
+				<!--<img src="assets/img/disclaimer.png">-->
+
+			<div class="line"></div>
+				<br>
+				<a href="theveil">
+					<img src="assets/img/button.png">
+				</a>
+				
+</div>
+</div>
+</div>
+
+
+<div class="bg-fix"></div>
+
+	<script src="assets/js/vendor.js"></script>
+	<script type="text/javascript" src="assets/js/main.js"></script>
+
+</body>
+</html>
